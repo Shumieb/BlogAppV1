5d52ed76341111e0c96287329fc71782ffbea318
Shumieb <shumiebc@gmail.com>
2024-04-26 20:22:38 +0100

    react icons installed

--- src/BlogCard.jsx
+++ src/BlogCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function BlogCard() {
     return (
-        <div className="card px-2 py-2 shadow-sm mb-3">
+        <div className="card px-2 pt-2 shadow-sm mb-3">
             <div className="card-body">
                 <p className="card-title text-decoration-underline">Blog One</p>
                 <p className="card-text font-weight-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum consequuntur,
@@ -10,8 +10,10 @@ function BlogCard() {
                     iure rem aliquid tenetur architecto vel facere dolorem veniam doloremque?
                     Dolorum optio, autem ea nemo exercitationem, nulla harum necessitatibus dicta quia deleniti non!
                 </p>
-                <div className="card-body">
-                    <p>20 Likes</p>
+                <div className="card-body d-flex flex-row justify-content-between align-items-center p-0 m-0">
+                    <div className="pt-3">
+                        <p><span>20</span> Likes</p>
+                    </div>
                     <Link to="/blog/blog-one" class="card-link btn btn-primary">View</Link>
                 </div>
             </div>
--- src/HomeSearchBar.jsx
+++ src/HomeSearchBar.jsx
@@ -3,7 +3,11 @@ function HomeSearchBar() {
         <div className="card shadow-sm">
             <div className="card-body">
                 <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <div className="input-group input-group-lg">
+                        <span className="input-group-text" id="inputGroup-sizing-lg">Large</span>
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    </div>
+
                     <button class="btn btn-primary" type="submit">Search</button>
                 </form>
             </div>
